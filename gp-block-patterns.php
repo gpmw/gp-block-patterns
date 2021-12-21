37a03a16b22dc03ce313736b9b68034b96e5bd95
Glenn Pearson <glenn.pearson@automattic.com>
2021-12-21 14:03:49 -0700

    Update and rename jc-block-patterns.php to gp-block-patterns.php
    CHanged this over to my name but still credit to Joe Casabona

--- gp-block-patterns.php
+++ gp-block-patterns.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:      Joe’s Block Patterns
- * Plugin URI:       https://casabona.org
+ * Plugin Name:      Glenn’s Block Patterns
+ * Plugin URI:       https://wpglenn.tech
  * Description:      This is a simple plugin with some common block patterns I’m using. 
  * Version:           1.0.0
- * Author:            Joe Casabona
- * Author URI:     https:/casabona.org/
+ * Author:            Glenn Pearson
+ * Author URI:     https://wpglenn.tech
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
@@ -18,6 +18,6 @@
  /**
   * Currently plugin version.
   */
- define( 'JC_BLOCK_PATTERNS_VERSION', '1.0.0' );
+ define( 'GP_BLOCK_PATTERNS_VERSION', '1.0.0' );
  
 require_once( 'inc/patterns.php' );
